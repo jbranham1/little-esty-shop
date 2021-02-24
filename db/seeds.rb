f7f36a64d306db172a3cfc81b5a53282d9c2f6d4
Gvieve <gnuebel84@yahoo.com>
2021-02-24 13:13:59 -0700

    Reset pk keys in test env seed, add new tests after seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 
-Rake::Task['fixtures:csv_load:all'].execute if Rails.env.test?
+if Rails.env.test?
+  Rake::Task['fixtures:csv_load:all'].execute
+
+  ActiveRecord::Base.connection.tables.each do |t|
+    ActiveRecord::Base.connection.reset_pk_sequence!(t)
+  end
+end 
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,17 +7,39 @@ RSpec.describe Customer, type: :model do
   end
 
   describe 'class methods' do
+    describe '::top_5_customers_with_success' do
+      it "gets the top 5 customers with successful transactions overall" do
+        results = Customer.top_5_customers_with_success
+
+        # expect(results.size).to eq(5)
+        expect(results.first.first_name).to eq("Leo")
+        expect(results.first.transaction_count).to eq(5)
+        expect(results.last.last_name).to eq("Zulauf")
+        expect(results.last.transaction_count).to eq(4)
+        expect(results.include?("Mariah")).to eq(false)
+      end
+    end
+
     describe '::top_customer_by_merchant' do
       it "gets the top 5 customers with successful transactions for a specific merchant" do
+        merchant = Merchant.all.first
+        results = Customer.top_customer_by_merchant(merchant.id)
 
-        expect(file_fixture('merchants_test.csv').size).to eq(1405)
+        # expect(results.size).to eq(5)
+        expect(results.first.first_name).to eq("Parker")
+        expect(results.first.transaction_count).to eq(8)
+        expect(results.last.last_name).to eq("Erdman")
+        expect(results.last.transaction_count).to eq(4)
+        expect(results.include?("Mariah")).to eq(false)
       end
     end
   end
+
   describe 'instance methods' do
     describe '::full_name' do
       it "combines first and last name" do
-        customer = create(:customer)
+        customer = Customer.find(4)
+
         expect(customer.full_name).to eq("#{customer.first_name} #{customer.last_name}")
       end
     end

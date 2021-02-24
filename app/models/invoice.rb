bdfb2ea4111fb45f0de47beb7922f6710c8d2b33
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-02-24 00:15:21 -0700

    Finished Admin Dashboard but need tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,7 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
 
   enum status: [:in_progress, :cancelled, :completed]
+
+  scope :incomplete_invoices, -> { includes(:invoice_items).where.not(status: 2).distinct.order(:created_at)}
+
 end

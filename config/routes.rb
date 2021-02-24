a87dff1c53b8d01b95e8c2e7229a1bf0d2532ea2
Jenny Branham <branhamjenny@gmail.com>
2021-02-23 20:02:45 -0600

    Add merchant dashboard and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-    resources :merchants, :invoices
+    resources :merchants, :invoices, :dashboard
   end
 
-  resources :merchants do
-    resources :invoices, :items, :dashboard
+  resources :merchant do
+    resources :invoices, :items
+    resources :dashboard, only: [:index]
   end
 
   resources :customers, :invoice_items, :transactions

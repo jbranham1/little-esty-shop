c9392092058d9831f252242d68fe3ae51079dc79
Jenny Branham <branhamjenny@gmail.com>
2021-02-23 20:50:17 -0600

    Add top cutstomer by merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,14 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices, dependent: :destroy
+
+  def self.top_customer_by_merchant(merchant_id)
+    joins(invoices: [:transactions, :items])
+    .select('customers.*, count(transactions.id) as transaction_count')
+    .where(transactions: {result: 0})
+    .where(items: {merchant_id: merchant_id})
+    .group(:id)
+    .order(transaction_count: :desc)
+    .limit(5)
+  end
 end

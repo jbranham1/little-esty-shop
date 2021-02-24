890bfca76a1d08880f58c3e9d015b165b57875a1
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-02-23 19:50:42 -0700

    Added successful transactions to admin dashboard

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,13 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices, dependent: :destroy
+
+  def full_name
+     [self.first_name, self.last_name].join(' ')
+  end
+
+  def self.top_5_customers_with_success
+    select('customers.first_name, customers.last_name, count(transactions.id) as transaction_count').joins(invoices: :transactions).where(transactions: {result: 0}).group(:id).order('transaction_count desc').limit(5)
+  end
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
   validates_presence_of :credit_card_number
   enum result: [:success, :failed]
+
 end

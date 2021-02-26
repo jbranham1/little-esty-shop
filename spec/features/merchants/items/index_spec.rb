65ea9f9dfdba1c51de0dcd7a95d01cd7a60b35fa
Gvieve <gnuebel84@yahoo.com>
2021-02-26 00:12:11 -0700

    Add additonal feature test for merch items index

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -12,10 +12,19 @@ RSpec.describe 'As a merchant, when I vist my Merchant Items Index Page' do
 
     within '.all-items' do
       expect(page.all('a', text: 'Item').count).to eq(26)
+      expect(page).to have_content(@items.first.name)
     end
   end
 
-  it 'each item name is a link to that item show page' do
+  it 'I do not see items for another merchant' do
+    visit merchant_items_path(@merchant)
+    merchant2 = Merchant.second
+    item = merchant2.items.first
+
+    expect(page).to_not have_content("#{item.name}")
+  end
+
+  it "each item name is a link to that item's show page" do
     item = @merchant.items.first
     visit merchant_items_path(@merchant)
 

9db6e094bae246860adc4a0e3ef74e003f14adbb
Gvieve <gnuebel84@yahoo.com>
2021-02-26 19:54:54 -0700

    Create controller actions and merchant edit view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,29 @@ class ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      @item.update!(item_params)
+      flash[:notice] = "#{@item.name} successfully updated"
+      render :show
+    else
+      flash[:errors] = @item.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :status)
+  end
 end

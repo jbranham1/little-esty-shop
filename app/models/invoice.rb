c8ca9595335e2c6a188fe688850bf5b76b6fb443
Jenny Branham <branhamjenny@gmail.com>
2021-02-28 14:41:44 -0600

    Delete unused code

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,12 +9,7 @@ class Invoice < ApplicationRecord
 
   scope :incomplete_invoices, -> { includes(:invoice_items).where.not(status: 2).distinct.order(:created_at)}
 
-  # scope :invoices_with_successful_transactions
   def total_revenue
     "$" + sprintf("%.2f", invoice_items.sum(&:revenue))
-  end
-
-  def distinct_status
-    [:in_progress, :cancelled, :completed]
   end
 end

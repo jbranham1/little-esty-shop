eb4a27b6bdc9cafa01e5340823e787efc45a82d8
Jenny Branham <branhamjenny@gmail.com>
2021-03-09 10:21:17 -0600

    add Bulk discounts to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@ if Rails.env.test?
   ActiveRecord::Base.connection.tables.each do |t|
     ActiveRecord::Base.connection.reset_pk_sequence!(t)
   end
-end 
+end
+bulk_discount1 = BulkDiscount.create(merchant_id: 1, percentage_discount: 20, quantity_threshold:10)
+bulk_discount2 = BulkDiscount.create(merchant_id: 1, percentage_discount: 30, quantity_threshold:15)
